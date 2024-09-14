f44d078e75a998da6a9f200dcaff0ede62c1d239
Cem Ege <cemege.kilincoglu@arabam.com>
2024-09-14 19:41:53 +0300

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by cemegeArabam on 14.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
